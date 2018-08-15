fcf674976f6fdd0931887da2024a89641d984f38
Duke Jake Morgan <dukejakemorgan@gmail.com>
2018-08-15 20:46:05 +0100

    Made LedgerHandler more asynchronous.

--- src/main/java/cm/pvp/voyagepvp/voyagecore/features/veconomy/LedgerHandler.java
+++ src/main/java/cm/pvp/voyagepvp/voyagecore/features/veconomy/LedgerHandler.java
@@ -3,41 +3,28 @@ package cm.pvp.voyagepvp.voyagecore.features.veconomy;
 import cm.pvp.voyagepvp.voyagecore.features.veconomy.accounts.ledger.entry.PlayerLedgerEntry;
 import cm.pvp.voyagepvp.voyagecore.features.veconomy.accounts.ledger.entry.SharedLedgerEntry;
 import cm.pvp.voyagepvp.voyagecore.features.veconomy.response.Action;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Lists;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 public class LedgerHandler
 {
     private DataHandler handler;
-    private Cache<UUID, ArrayList<SharedLedgerEntry>> sharedLedgers = CacheBuilder.newBuilder().expireAfterWrite(3, TimeUnit.MINUTES).build();
-    private Cache<UUID, ArrayList<PlayerLedgerEntry>> playerLedgers = CacheBuilder.newBuilder().expireAfterWrite(3, TimeUnit.MINUTES).build();
-
     public LedgerHandler(DataHandler handler)
     {
         this.handler = handler;
     }
 
-    public LinkedList<SharedLedgerEntry> getLedgersFrom(UUID sharedAccountId, Date date)
+    public CompletableFuture<LinkedList<SharedLedgerEntry>> getLedgersFrom(UUID sharedAccountId, Date date)
     {
-        if (sharedLedgers.asMap().containsKey(sharedAccountId)) {
-            return sharedLedgers.getIfPresent(sharedAccountId).stream().filter(entry -> entry.getDate().equals(date)).collect(Collectors.toCollection(Lists::newLinkedList));
-        }
 
-
-        CompletableFuture<LinkedList<SharedLedgerEntry>> future = CompletableFuture.supplyAsync(() -> {
+        return CompletableFuture.supplyAsync(() -> {
             try {
                 Connection connection = handler.getSource().getConnection();
                 PreparedStatement statement = connection.prepareStatement("select * from shared_account_ledgers where accountId=? and date=?");
@@ -71,19 +58,11 @@ public class LedgerHandler
 
             return ledgerSection;
         });
-
-        try {
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
-    public LinkedList<SharedLedgerEntry> getEntireLedger(UUID sharedAccountId)
+    public CompletableFuture<LinkedList<SharedLedgerEntry>> getEntireLedger(UUID sharedAccountId)
     {
-        CompletableFuture<LinkedList<SharedLedgerEntry>> future = CompletableFuture.supplyAsync(() -> {
+        return CompletableFuture.supplyAsync(() -> {
             try {
                 Connection connection = handler.getSource().getConnection();
                 PreparedStatement statement = connection.prepareStatement("select * from shared_account_ledgers where accountId=?");
@@ -116,19 +95,11 @@ public class LedgerHandler
 
             return ledger;
         });
-
-        try {
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
-    public LinkedList<PlayerLedgerEntry> getPersonalLedgersFrom(UUID playerId, Date date)
+    public CompletableFuture<LinkedList<PlayerLedgerEntry>> getPersonalLedgersFrom(UUID playerId, Date date)
     {
-        CompletableFuture<LinkedList<PlayerLedgerEntry>> future = CompletableFuture.supplyAsync(() -> {
+        return CompletableFuture.supplyAsync(() -> {
             try {
                 Connection connection = handler.getSource().getConnection();
                 PreparedStatement statement = connection.prepareStatement("select * from shared_account_ledgers where accountId=? and date=?");
@@ -162,19 +133,11 @@ public class LedgerHandler
 
             return ledgerSection;
         });
-
-        try {
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
-    public LinkedList<PlayerLedgerEntry> getEntirePersonalLedger(UUID playerId)
+    public CompletableFuture<LinkedList<PlayerLedgerEntry>> getEntirePersonalLedger(UUID playerId)
     {
-        CompletableFuture<LinkedList<PlayerLedgerEntry>> future = CompletableFuture.supplyAsync(() -> {
+        return CompletableFuture.supplyAsync(() -> {
             try {
                 Connection connection = handler.getSource().getConnection();
                 PreparedStatement statement = connection.prepareStatement("select * from shared_account_ledgers where accountId=?");
@@ -207,13 +170,5 @@ public class LedgerHandler
 
             return ledger;
         });
-
-        try {
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 }
