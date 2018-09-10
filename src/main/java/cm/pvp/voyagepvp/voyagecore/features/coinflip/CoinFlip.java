ac5bb82edc64dab2e9c99c0e07ed891f27eb217f
TheMasteredPanda <dukejakemorgan@gmail.com>
2018-09-10 11:43:01 +0100

    Created the CoinFlip module.

---
+++ src/main/java/cm/pvp/voyagepvp/voyagecore/features/coinflip/CoinFlip.java
@@ -0,0 +1,52 @@
+package cm.pvp.voyagepvp.voyagecore.features.coinflip;
+
+import cm.pvp.voyagepvp.voyagecore.Feature;
+import cm.pvp.voyagepvp.voyagecore.VoyageCore;
+import cm.pvp.voyagepvp.voyagecore.api.command.VoyageCommand;
+import cm.pvp.voyagepvp.voyagecore.api.config.wrapper.ConfigPopulate;
+import cm.pvp.voyagepvp.voyagecore.api.locale.Format;
+import org.bukkit.command.CommandSender;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class CoinFlip extends Feature
+{
+    @ConfigPopulate("features.coinflip.messages.flipped")
+    private String coinFlipMessage;
+
+    public CoinFlip(VoyageCore instance)
+    {
+        super(instance, "CoinFlip", 1.0);
+
+        try {
+            getInstance().getMainConfig().populate(this);
+        } catch (OperationNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected boolean enable() throws Exception
+    {
+        getInstance().register(new FlipCommand());
+        return true;
+    }
+
+    public class FlipCommand extends VoyageCommand
+    {
+        private Random r = new Random();
+
+        public FlipCommand()
+        {
+            super(null, "voyagecore.coinflip.flip", "Flip a coin.", true, "coinflip.");
+        }
+
+        @Override
+        public void execute(CommandSender sender, VoyageCommand command, LinkedList<String> arguments)
+        {
+            sender.sendMessage(Format.colour(Format.format(coinFlipMessage, "{result};" + (r.nextInt(1) == 0 ? "Heads" : "Tails"))));
+        }
+    }
+}
