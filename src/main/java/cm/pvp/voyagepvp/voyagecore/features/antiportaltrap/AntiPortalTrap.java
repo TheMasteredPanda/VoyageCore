1e5ec0fef1b5632e30af7d188ba47c4fd884cd31
TheMasteredPanda <dukejakemorgan@gmail.com>
2018-09-16 10:01:24 +0100

    Added AntiPortalTrap fix.

---
+++ src/main/java/cm/pvp/voyagepvp/voyagecore/features/antiportaltrap/AntiPortalTrap.java
@@ -0,0 +1,38 @@
+package cm.pvp.voyagepvp.voyagecore.features.antiportaltrap;
+
+import cm.pvp.voyagepvp.voyagecore.Feature;
+import cm.pvp.voyagepvp.voyagecore.VoyageCore;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class AntiPortalTrap extends Feature implements Listener
+{
+    public AntiPortalTrap(VoyageCore instance)
+    {
+        super(instance, "AntiPortalTrap", 1.0);
+    }
+
+    @Override
+    protected boolean enable() throws Exception
+    {
+        Bukkit.getPluginManager().registerEvents(this, getInstance());
+        return true;
+    }
+
+    @EventHandler
+    public void on(PlayerJoinEvent e)
+    {
+        Location location = e.getPlayer().getLocation();
+        Material bottom = location.subtract(0D, 1D, 0D).getBlock().getType();
+        Material top = location.add(0D, 1D, 0D).getBlock().getType();
+
+        if (bottom.equals(Material.PORTAL) || bottom.equals(Material.ENDER_PORTAL) || bottom.equals(Material.ENDER_PORTAL_FRAME) || top.equals(Material.PORTAL) || top.equals(Material.ENDER_PORTAL) || top.equals(Material.ENDER_PORTAL_FRAME)) {
+            Location spawnLocation = e.getPlayer().getWorld().getSpawnLocation();
+            e.getPlayer().teleport(spawnLocation);
+        }
+    }
+}
